ec783c6fee4999be202eecf4717cac397389f631
Steven De Jesus <sdejesu1>
2024-02-27 07:04:37 -0500

    reversing char success: only enemy tested, next is player

--- index.js
+++ index.js
@@ -76,8 +76,57 @@ const player = new Fighter({
     }
 })
 
+/*
+const player = new Fighter({
+    position:{x: 200, y: 0},
+    velocity: {x: 0, y: 0},
+    imageSrc: "./edo_assets/Luffy/Idle.png",
+    framesMax: 7,
+    scale: 2.2,
+    offset: {x: 100, y: 55},
+    sprites: {
+        idle: {
+            imageSrc: "./edo_assets/Luffy/Idle.png",
+            framesMax: 7
+        },
+        run: {
+            imageSrc: "./edo_assets/Luffy/Run.png",
+            framesMax: 6
+        },
+        jump: {
+            imageSrc: "./edo_assets/Luffy/Jump.png",
+            framesMax: 6
+        },
+        fall: {
+            imageSrc: "./edo_assets/Luffy/Fall.png",
+            framesMax: 5
+        },
+        attack1: {
+            imageSrc: "./edo_assets/Luffy/Attack1.png",
+            framesMax: 10
+        },
+        takeHit: {
+            imageSrc: "./edo_assets/Luffy/Take Hit.png",
+            framesMax: 7
+        },
+        death: {
+            imageSrc: "./edo_assets/Luffy/Death.png",
+            framesMax: 4
+        }
+    },
+    attackBox: {
+        offset: {
+            x: 80,
+            y: 60
+        },
+        width: 155,
+        height: 50
+    }
+})
+*/
+
 const enemy = new Fighter({
-    position:{x: 400, y: 100},
+    position:{x: 800, y: 100},
     velocity: {x: 0, y: 0},
     color: 'blue',
     imageSrc: "./edo_assets/kenji/Idle.png",
@@ -112,6 +161,36 @@ const enemy = new Fighter({
         death: {
             imageSrc: "./edo_assets/kenji/Death.png",
             framesMax: 7
+        },
+
+        // reverse images
+        idleRev: {
+            imageSrc: "./edo_assets/kenji/Idlereverse.png",
+            framesMax: 4
+        },
+        runRev: {
+            imageSrc: "./edo_assets/kenji/Runreverse.png",
+            framesMax: 8
+        },
+        jumpRev: {
+            imageSrc: "./edo_assets/kenji/Jumpreverse.png",
+            framesMax: 2
+        },
+        fallRev: {
+            imageSrc: "./edo_assets/kenji/Fallreverse.png",
+            framesMax: 2
+        },
+        attack1Rev: {
+            imageSrc: "./edo_assets/kenji/Attack1reverse.png",
+            framesMax: 4
+        },
+        takeHitRev: {
+            imageSrc: "./edo_assets/kenji/Take hitreverse.png",
+            framesMax: 3
+        },
+        deathRev: {
+            imageSrc: "./edo_assets/kenji/Deathreverse.png",
+            framesMax: 7
         }
     },
     attackBox: {
@@ -123,8 +202,6 @@ const enemy = new Fighter({
         height: 50
     }
 })
-
-console.log(player)
 
 const keys = {
     a: {
@@ -148,8 +225,6 @@ const keys = {
         pressed: false
     }
 }
-
-// rectangular collision fxn
 
 
 decreaseTimer()
@@ -180,27 +255,67 @@ function animate() {
         player.switchSprite('idle')
     }
 
-    // jumping
+    // player jumping and reversing
     if (player.velocity.y < 0) {
         player.switchSprite('jump')
     } else if (player.velocity.y > 0) {
         player.switchSprite('fall')
     }
-    if (enemy.velocity.y < 0) {
-        enemy.switchSprite('jump')
-    } else if (player.velocity.y > 0) {
-        enemy.switchSprite('fall')
+
+    /*
+    if (enemy.isReversed) {
+            enemy.switchSprite('jumpRev');
+        } else {
+            enemy.switchSprite('jump');
+        }
+
+    if (enemy.isReversed) {
+            enemy.switchSprite('fallRev');
+        } else {
+            enemy.switchSprite('fall');
+        }
+     */
+
+    // enemy jumping
+    if (enemy.isReversed){
+        // enemy jumping and reversing
+        if (enemy.velocity.y < 0) {
+            enemy.switchSprite('jumpRev')
+            console.log('jumpRev')
+        } else if (enemy.velocity.y > 0) {
+            enemy.switchSprite('fallRev')
+            console.log('fallRev')
+        }
+    } else {
+        if (enemy.velocity.y < 0) {
+            enemy.switchSprite('jump')
+            console.log('jump')
+        } else if (enemy.velocity.y > 0) {
+            enemy.switchSprite('fall')
+            console.log('fall')
+        }
     }
+
 
     // enemy movement
     if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
         enemy.velocity.x = -5
         enemy.switchSprite('run')
+        console.log('run')
     } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
         enemy.velocity.x = 5
-        enemy.switchSprite('run')
-    } else {
-        enemy.switchSprite('idle')
+        enemy.switchSprite('runRev')
+        console.log('runRev')
+    } else if (enemy.velocity.y === 0) {
+        if (enemy.isReversed) {
+            enemy.switchSprite('idleRev')
+            console.log('idleRev')
+        } else {
+            enemy.switchSprite('idle')
+            console.log('idle')
+        }
+    }{
+
     }
 
     // detecting for collision and enemy gets hit
@@ -222,7 +337,7 @@ function animate() {
         && enemy.isAttacking && enemy.framesCurrent === 2) {
         player.takeHit()
         enemy.isAttacking = false
-        gsap.to('#enemyHealth', {
+        gsap.to('#playerHealth', {
             width: player.health + '%'
         })
     }
@@ -250,7 +365,6 @@ window.addEventListener('keydown', (event) => {
                 break
             case 'a':
                 keys.a.pressed = true
-                player.lastKey = 'a'
                 break
             case 'w':
                 player.velocity.y = -20
@@ -267,10 +381,12 @@ window.addEventListener('keydown', (event) => {
             case 'ArrowRight':
                 keys.ArrowRight.pressed = true
                 enemy.lastKey = 'ArrowRight'
+                enemy.isReversed = true
                 break
             case 'ArrowLeft':
                 keys.ArrowLeft.pressed = true
                 enemy.lastKey = 'ArrowLeft'
+                enemy.isReversed = false
                 break
             case 'ArrowUp':
                 enemy.velocity.y = -20
--- js/classes.js
+++ js/classes.js
@@ -26,9 +26,10 @@ class Sprite {
             this.image.height * this.scale
         )
     }
+
     animateFrames() {
         this.framesElapsed++
-        if (this.framesElapsed % this.framesHold === 0){
+        if (this.framesElapsed % this.framesHold === 0) {
             if (this.framesCurrent < this.framesMax - 1) {
                 this.framesCurrent++
             } else {
@@ -36,14 +37,25 @@ class Sprite {
             }
         }
     }
-    update(){
+
+    update() {
         this.draw()
         this.animateFrames()
     }
 }
 
 class Fighter extends Sprite {
-    constructor({position, velocity, color = 'red', imageSrc, scale = 1, framesMax = 1, offset = {x: 0, y: 0}, sprites, attackBox = {offset: {}, width: undefined, height: undefined}}) {
+    constructor({
+                    position,
+                    velocity,
+                    color = 'red',
+                    imageSrc,
+                    scale = 1,
+                    framesMax = 1,
+                    offset = {x: 0, y: 0},
+                    sprites,
+                    attackBox = {offset: {}, width: undefined, height: undefined}
+                }) {
         super({
             position,
             imageSrc,
@@ -72,28 +84,38 @@ class Fighter extends Sprite {
         this.framesHold = 5
         this.sprites = sprites
         this.dead = false
+        this.isReversed = false
 
-        for (const sprite in this.sprites){
+        for (const sprite in this.sprites) {
             sprites[sprite].image = new Image()
             sprites[sprite].image.src = sprites[sprite].imageSrc
         }
     }
 
 
-    update(){
+    update() {
         this.draw()
         if (!this.dead) {
             this.animateFrames()
+            this.reverseChar()
         }
+
         this.attackBox.position.x = this.position.x + this.attackBox.offset.x
         this.attackBox.position.y = this.position.y + this.attackBox.offset.y
 
+        if (this.isReversed) {
+            this.attackBox.position.x += -1 * this.attackBox.offset.x + 70
+        }
+
         // testing the attack box
-        // c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
+        //c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
+
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
-        if (this.position.y + this.height + this.velocity.y >= (canvas.height - 96)){
+
+        // stopping where the sprite falls a tiny bit short, to keep sprite integrity when falling
+        if (this.position.y + this.height + this.velocity.y >= (canvas.height - 96)) {
             this.velocity.y = 0
             this.position.y = 330
         } else {
@@ -101,91 +123,193 @@ class Fighter extends Sprite {
         }
     }
 
-    attack(){
-        this.switchSprite('attack1')
+
+    reverseChar() {
+        if (this.isReversed) {
+            console.log("Char is reversed")
+        }
+    }
+
+    attack() {
+        if (this.isReversed === true) {
+            this.switchSprite('attack1Rev')
+        } else {
+            this.switchSprite('attack1')
+        }
         this.isAttacking = true
     }
+
     takeHit() {
-        this.health -= 20
+        this.health -= 5
         if (this.health <= 0) {
-            this.switchSprite('death')
+            if (this.isReversed === true) {
+                this.switchSprite('deathRev')
+            } else {
+                this.switchSprite('death')
+            }
         } else {
-            (this.switchSprite('takeHit'))
+            if (this.isReversed === true) {
+                this.switchSprite('takeHitRev')
+            } else {
+                this.switchSprite('takeHit')
+            }
         }
     }
 
     switchSprite(sprite) {
-        // overriding all other animations with death animation
-        if (this.image === this.sprites.death.image) {
-            if (this.framesCurrent === this.sprites.death.framesMax-1) {
-                this.dead = true
+        // cases for if player is reversed
+        if (this.isReversed === true) {
+            // overriding all other animations with death animation
+            if (this.image === this.sprites.deathRev.image) {
+                if (this.framesCurrent === this.sprites.deathRev.framesMax - 1) {
+                    this.dead = true
+                }
+                return
             }
-            return
-        }
-        // overriding all other animations with attack animation
-        if (this.image === this.sprites.attack1.image && this.framesCurrent < this.sprites.attack1.framesMax - 1) {
-            return
-        }
-        // overriding all other animations with take hit animation
-        if (this.image === this.sprites.takeHit.image && this.framesCurrent < this.sprites.takeHit.framesMax - 1) {
-            return
-        }
-        switch (sprite) {
-            case 'idle':
-                if (this.image !== this.sprites.idle.image){
-                    this.image = this.sprites.idle.image
-                    this.framesMax = this.sprites.idle.framesMax
-                    this.framesCurrent = 0
-                }
-                break
+            // overriding all other animations with attack animation
+            if (this.image === this.sprites.attack1Rev.image && this.framesCurrent < this.sprites.attack1Rev.framesMax - 1) {
+                return
+            }
+            // overriding all other animations with take hit animation
+            if (this.image === this.sprites.takeHitRev.image && this.framesCurrent < this.sprites.takeHitRev.framesMax - 1) {
+                return
+            }
+            switch (sprite) {
+                case 'idleRev':
+                    if (this.image !== this.sprites.idleRev.image) {
+                        this.image = this.sprites.idleRev.image
+                        this.framesMax = this.sprites.idleRev.framesMax
+                        this.framesCurrent = 0
+                    }
+                    break
 
-            case 'run':
-                if (this.image !== this.sprites.run.image){
-                    this.image = this.sprites.run.image
-                    this.framesMax = this.sprites.run.framesMax
-                    this.framesCurrent = 0
-                }
-                break
+                case 'runRev':
+                    if (this.image !== this.sprites.runRev.image) {
+                        this.image = this.sprites.runRev.image
+                        this.framesMax = this.sprites.runRev.framesMax
+                        this.framesCurrent = 0
+                    }
+                    break
 
-            case 'jump':
-                if (this.image !== this.sprites.jump.image){
-                    this.image = this.sprites.jump.image
-                    this.framesMax = this.sprites.jump.framesMax
-                    this.framesCurrent = 0
-                }
-                break
+                case 'jumpRev':
+                    if (this.image !== this.sprites.jumpRev.image) {
+                        this.image = this.sprites.jumpRev.image
+                        this.framesMax = this.sprites.jumpRev.framesMax
+                        this.framesCurrent = 0
+                        console.log('char should be jumping rev')
+                    }
+                    break
 
-            case 'fall':
-                if (this.image !== this.sprites.fall.image){
-                    this.image = this.sprites.fall.image
-                    this.framesMax = this.sprites.fall.framesMax
-                    this.framesCurrent = 0
-                }
-                break
+                case 'fallRev':
+                    if (this.image !== this.sprites.fallRev.image) {
+                        this.image = this.sprites.fallRev.image
+                        this.framesMax = this.sprites.fallRev.framesMax
+                        this.framesCurrent = 0
+                        console.log('char should be falling rev')
+                    }
+                    break
 
-            case 'attack1':
-                if (this.image !== this.sprites.attack1.image){
-                    this.image = this.sprites.attack1.image
-                    this.framesMax = this.sprites.attack1.framesMax
-                    this.framesCurrent = 0
-                }
-                break
+                case 'attack1Rev':
+                    if (this.image !== this.sprites.attack1Rev.image) {
+                        this.image = this.sprites.attack1Rev.image
+                        this.framesMax = this.sprites.attack1Rev.framesMax
+                        this.framesCurrent = 0
+                    }
+                    break
 
-            case 'takeHit':
-                if (this.image !== this.sprites.takeHit.image){
-                    this.image = this.sprites.takeHit.image
-                    this.framesMax = this.sprites.takeHit.framesMax
-                    this.framesCurrent = 0
-                }
-                break
+                case 'takeHitRev':
+                    if (this.image !== this.sprites.takeHitRev.image) {
+                        this.image = this.sprites.takeHitRev.image
+                        this.framesMax = this.sprites.takeHitRev.framesMax
+                        this.framesCurrent = 0
+                    }
+                    break
 
-            case 'death':
-                if (this.image !== this.sprites.death.image){
-                    this.image = this.sprites.death.image
-                    this.framesMax = this.sprites.death.framesMax
-                    this.framesCurrent = 0
+                case 'deathRev':
+                    if (this.image !== this.sprites.deathRev.image) {
+                        this.image = this.sprites.deathRev.image
+                        this.framesMax = this.sprites.deathRev.framesMax
+                        this.framesCurrent = 0
+                    }
+                    break
+            }
+            // else the player is not reversed
+        } else {
+            // overriding all other animations with death animation
+            if (this.image === this.sprites.death.image) {
+                if (this.framesCurrent === this.sprites.death.framesMax - 1) {
+                    this.dead = true
                 }
-                break
+                return
+            }
+            // overriding all other animations with attack animation
+            if (this.image === this.sprites.attack1.image && this.framesCurrent < this.sprites.attack1.framesMax - 1) {
+                return
+            }
+            // overriding all other animations with take hit animation
+            if (this.image === this.sprites.takeHit.image && this.framesCurrent < this.sprites.takeHit.framesMax - 1) {
+                return
+            }
+            switch (sprite) {
+                case 'idle':
+                    if (this.image !== this.sprites.idle.image) {
+                        this.image = this.sprites.idle.image
+                        this.framesMax = this.sprites.idle.framesMax
+                        this.framesCurrent = 0
+                    }
+                    break
+
+                case 'run':
+                    if (this.image !== this.sprites.run.image) {
+                        this.image = this.sprites.run.image
+                        this.framesMax = this.sprites.run.framesMax
+                        this.framesCurrent = 0
+                    }
+                    break
+
+                case 'jump':
+                    if (this.image !== this.sprites.jump.image) {
+                        this.image = this.sprites.jump.image
+                        this.framesMax = this.sprites.jump.framesMax
+                        this.framesCurrent = 0
+                        console.log('char should be jumping')
+                    }
+                    break
+
+                case 'fall':
+                    if (this.image !== this.sprites.fall.image) {
+                        this.image = this.sprites.fall.image
+                        this.framesMax = this.sprites.fall.framesMax
+                        this.framesCurrent = 0
+                        console.log('char should be falling')
+                    }
+                    break
+
+                case 'attack1':
+                    if (this.image !== this.sprites.attack1.image) {
+                        this.image = this.sprites.attack1.image
+                        this.framesMax = this.sprites.attack1.framesMax
+                        this.framesCurrent = 0
+                    }
+                    break
+
+                case 'takeHit':
+                    if (this.image !== this.sprites.takeHit.image) {
+                        this.image = this.sprites.takeHit.image
+                        this.framesMax = this.sprites.takeHit.framesMax
+                        this.framesCurrent = 0
+                    }
+                    break
+
+                case 'death':
+                    if (this.image !== this.sprites.death.image) {
+                        this.image = this.sprites.death.image
+                        this.framesMax = this.sprites.death.framesMax
+                        this.framesCurrent = 0
+                    }
+                    break
+
+            }
         }
     }
 }
